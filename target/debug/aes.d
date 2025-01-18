8cdaa175cbf87551259a18b61abaad9232dcf123
Merijn <merijn.laks@student.uva.nl>
2025-01-18 19:24:40 +0100

    cleanup

--- target/debug/aes.d
+++
@@ -1,1 +0,0 @@
-/home/ocelot/code/school/INTS/aes_rust/target/debug/aes: /home/ocelot/code/school/INTS/aes_rust/src/main.rs /home/ocelot/code/school/INTS/aes_rust/src/sbox.rs
--- target/debug/deps/aes-5fc287ebe4c818c5.d
+++
@@ -1,6 +0,0 @@
-/home/ocelot/code/school/INTS/aes_rust/target/debug/deps/aes-5fc287ebe4c818c5: src/main.rs src/sbox.rs
-
-/home/ocelot/code/school/INTS/aes_rust/target/debug/deps/aes-5fc287ebe4c818c5.d: src/main.rs src/sbox.rs
-
-src/main.rs:
-src/sbox.rs:
